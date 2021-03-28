f9c33f718e384a1dd831ccd4824091e26c6e33b8
agustin del valle <agusdelva.16@gmail.com>
2021-03-28 18:34:58 -0300

    second commit

--- src/App.js
+++ src/App.js
@@ -39,7 +39,7 @@ function App() {
           <input 
             type="text"
             className="search-bar"
-            placeholder="Search..."
+            placeholder="Ingrese ciudad o pais..."
             onChange={e => setQuery(e.target.value)}
             value={query}
             onKeyPress={search}
